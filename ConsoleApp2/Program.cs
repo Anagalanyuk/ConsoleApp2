7703a71e5e186143d03ced44dc8dfe6986c4e2f0
Anatoliy Anatoliy <anagalanyuk@gmail.com>
2018-04-15 10:06:27 +0300

    close #8

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,79 +7,84 @@ namespace TitleCapitalizationTool
 	{
 		public static void Main()
 		{
-			string toCapitalize = "Enter title to capitalize: ";
-			Console.Write(toCapitalize);
-			Console.ForegroundColor = ConsoleColor.Red;
-			string title = Console.ReadLine();
-			while(title.Length == 0)
+			int y = 0;
+			int x = 27;
+			while (true)
 			{
-				Console.SetCursorPosition(27, 0);
-				title = Console.ReadLine();
-			}
-			Console.ForegroundColor = ConsoleColor.Gray;
-			string capitalizedTitle = "Capitalized title: ";
-			Console.Write(capitalizedTitle);
-			Console.ForegroundColor = ConsoleColor.Green;
-			title = title.Trim();
-			title = title.ToLower();
-			string[] arrayTitle = title.Split(' ');
-			StringBuilder titleAdjustment = new StringBuilder();
-			arrayTitle[0] = char.ToUpper(arrayTitle[0][0]).ToString() + arrayTitle[0].Remove(0, 1);
-			arrayTitle[arrayTitle.Length - 1] = char.ToUpper(arrayTitle[arrayTitle.Length - 1][0]).ToString() + arrayTitle[arrayTitle.Length - 1].Remove(0, 1);
-			string[] exception = { "a", "an", "and" , "at", "but" , "by" , "for", "in","nor", "of", "on", "or", "out", "so" , "the" , "to" , "up" , "yet", };
-			bool result;
-			for (int i = 0; i < arrayTitle.Length; ++i)
-			{
-				result = true;
-				if (arrayTitle[i] != "")
+				string toCapitalize = "Enter title to capitalize: ";
+				Console.Write(toCapitalize);
+				Console.ForegroundColor = ConsoleColor.Red;
+			    string title = Console.ReadLine();
+				while (title.Length == 0)
 				{
-					for (int j = 0; j < exception.Length - 1; ++j)
+					Console.SetCursorPosition(x, y);
+					title = Console.ReadLine();
+				}
+				Console.ForegroundColor = ConsoleColor.Gray;
+				string capitalizedTitle = "Capitalized title: ";
+				Console.Write(capitalizedTitle);
+				Console.ForegroundColor = ConsoleColor.Green;
+				title = title.Trim();
+				title = title.ToLower();
+				string[] arrayTitle = title.Split(' ');
+				StringBuilder titleAdjustment = new StringBuilder();
+				arrayTitle[0] = char.ToUpper(arrayTitle[0][0]).ToString() + arrayTitle[0].Remove(0, 1);
+				arrayTitle[arrayTitle.Length - 1] = char.ToUpper(arrayTitle[arrayTitle.Length - 1][0]).ToString() + arrayTitle[arrayTitle.Length - 1].Remove(0, 1);
+				string[] exception = { "a", "an", "and", "at", "but", "by", "for", "in", "nor", "of", "on", "or", "out", "so", "the", "to", "up", "yet" };
+				for (int i = 0; i < arrayTitle.Length; ++i)
+				{
+					bool result = true;
+					if (arrayTitle[i] != "")
 					{
-						if (arrayTitle[i] == exception[j])
+						for (int j = 0; j < exception.Length - 1; ++j)
 						{
-							result = false;
-							break;
+							if (arrayTitle[i] == exception[j])
+							{
+								result = false;
+								break;
+							}
+						}
+						if (result)
+						{
+							arrayTitle[i] = char.ToUpper(arrayTitle[i][0]).ToString() + arrayTitle[i].Remove(0, 1);
+						}
+						titleAdjustment.Append(arrayTitle[i]);
+						titleAdjustment.Append(' ');
+					}
+				}
+				char[] punctuationMarks = { ',', ':', ';', '?', '.', '!', '-' };
+				StringBuilder newTitle = new StringBuilder();
+				for (int i = 0; i < titleAdjustment.Length - 1; ++i)
+				{
+					bool result = false;
+					for(int j = 0; j < punctuationMarks.Length; ++j)
+					{
+						if (titleAdjustment[i] == ' ' && titleAdjustment[i + 1] == punctuationMarks[j])
+						{
+							i += 1;
+						}
+						if (titleAdjustment[i] == punctuationMarks[j] && titleAdjustment[i + 1] != ' ')
+						{
+							result = true;
 						}
 					}
-					if (result)
+					if(titleAdjustment[i] == '-')
 					{
-						arrayTitle[i] = char.ToUpper(arrayTitle[i][0]).ToString() + arrayTitle[i].Remove(0, 1);
+						newTitle.Append(' ');
 					}
-					titleAdjustment.Append(arrayTitle[i]);
-					titleAdjustment.Append(' ');
-				}
-			}
-			StringBuilder newTitle = new StringBuilder();
-			for (int i = 0, j = 0; i < titleAdjustment.Length; ++i, ++j)
-			{
-				if (titleAdjustment[i] == ',' && titleAdjustment[i - 1] == ' ' || titleAdjustment[i] == ':' && titleAdjustment[i - 1] == ' ' || titleAdjustment[i] == ';' && titleAdjustment[i - 1] == ' ' ||
-					titleAdjustment[i] == '?' && titleAdjustment[i - 1] == ' ' || titleAdjustment[i] == '.' && titleAdjustment[i - 1] == ' ' || titleAdjustment[i] == '!' && titleAdjustment[i - 1] == ' ')
-				{
-					--j;
-					titleAdjustment.Length += 1;
-				}
-				titleAdjustment[j] = titleAdjustment[i];
-			}
-			for (int i = 0; i < titleAdjustment.Length; ++i)
-			{
-				if (titleAdjustment[i] == ',' && titleAdjustment[i + 1] != ' ' || titleAdjustment[i] == ':' && titleAdjustment[i + 1] != ' ' || titleAdjustment[i] == ';' && titleAdjustment[i + 1] != ' ' ||
-					 titleAdjustment[i] == '?' && titleAdjustment[i + 1] != ' ' || titleAdjustment[i] == '.' && titleAdjustment[i + 1] != ' ' || titleAdjustment[i] == '!' && titleAdjustment[i + 1] != ' ' && titleAdjustment[i + 1] != '!')
-				{
 					newTitle.Append(titleAdjustment[i]);
-					newTitle.Append(' ');
+					if(result)
+					{
+						newTitle.Append(' ');
+					    newTitle.Append(titleAdjustment[i + 1].ToString().ToUpper());
+					    i += 1;
+					}
 				}
-				else if (titleAdjustment[i] == '-' && titleAdjustment[i - 1] != ' ')
-				{
-					newTitle.Append(" - ");
-					titleAdjustment[i + 1] = char.ToUpper(titleAdjustment[i + 1]);
-				}
-				else
-				{
-					newTitle.Append(titleAdjustment[i]);
-				}
+				Console.WriteLine(newTitle);
+				Console.ResetColor();
+				Console.WriteLine();
+				y += 3;
 			}
-			Console.WriteLine(newTitle);
-			Console.ResetColor();
 		}
 	}
 }
