df6d241ffd6a51a19aaa053ce4ffe1def9d14dda
Anatoliy Anatoliy <anagalanyuk@gmail.com>
2018-04-16 22:03:56 +0300

    close #18 close #20

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,8 +7,8 @@ namespace TitleCapitalizationTool
 	{
 		public static void Main()
 		{
-			int y = 0;
-			int x = 27;
+			int coordinateCursorX = 27;
+			int coordinateCursorY = 0;
 			while (true)
 			{
 				string toCapitalize = "Enter title to capitalize: ";
@@ -17,7 +17,7 @@ namespace TitleCapitalizationTool
 			    string title = Console.ReadLine();
 				while (title.Length == 0)
 				{
-					Console.SetCursorPosition(x, y);
+					Console.SetCursorPosition(coordinateCursorX, coordinateCursorY);
 					title = Console.ReadLine();
 				}
 				Console.ForegroundColor = ConsoleColor.Gray;
@@ -30,15 +30,15 @@ namespace TitleCapitalizationTool
 				StringBuilder titleAdjustment = new StringBuilder();
 				arrayTitle[0] = char.ToUpper(arrayTitle[0][0]).ToString() + arrayTitle[0].Remove(0, 1);
 				arrayTitle[arrayTitle.Length - 1] = char.ToUpper(arrayTitle[arrayTitle.Length - 1][0]).ToString() + arrayTitle[arrayTitle.Length - 1].Remove(0, 1);
-				string[] exception = { "a", "an", "and", "at", "but", "by", "for", "in", "nor", "of", "on", "or", "out", "so", "the", "to", "up", "yet" };
+				string[] exceptions = { "a", "an", "and", "at", "but", "by", "for", "in", "nor", "of", "on", "or", "out", "so", "the", "to", "up", "yet" };
 				for (int i = 0; i < arrayTitle.Length; ++i)
 				{
 					bool result = true;
 					if (arrayTitle[i] != "")
 					{
-						for (int j = 0; j < exception.Length - 1; ++j)
+						for (int j = 0; j < exceptions.Length - 1; ++j)
 						{
-							if (arrayTitle[i] == exception[j])
+							if (arrayTitle[i] == exceptions[j])
 							{
 								result = false;
 								break;
@@ -56,7 +56,7 @@ namespace TitleCapitalizationTool
 				StringBuilder newTitle = new StringBuilder();
 				for (int i = 0; i < titleAdjustment.Length - 1; ++i)
 				{
-					bool result = false;
+					bool addSpace = false;
 					for (int j = 0; j < punctuationMarks.Length; ++j)
 					{
 						if (titleAdjustment[i] == ' ' && titleAdjustment[i + 1] == punctuationMarks[j])
@@ -65,7 +65,7 @@ namespace TitleCapitalizationTool
 						}
 						if (titleAdjustment[i] == punctuationMarks[j] && titleAdjustment[i + 1] != ' ')
 						{
-							result = true;
+							addSpace = true;
 						}
 					}
 					if (titleAdjustment[i] == '-')
@@ -73,14 +73,14 @@ namespace TitleCapitalizationTool
 						newTitle.Append(' ');
 					}
 					newTitle.Append(titleAdjustment[i]);
-					if (result)
+					if (addSpace)
 					{
 						newTitle.Append(' ');
 					    newTitle.Append(titleAdjustment[i + 1].ToString().ToUpper());
 					    i += 1;
 					}
 				}
-				y += 3;
+				coordinateCursorY += 3;
 				Console.WriteLine(newTitle);
 				Console.ResetColor();
 				Console.WriteLine();
