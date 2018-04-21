747877b953baf7307209b1b84dae11c762cf8dfc
Anatoliy Anatoliy <anagalanyuk@gmail.com>
2018-04-21 22:29:15 +0300

    close #23

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,59 +8,62 @@ namespace TitleCapitalizationTool
 		public static StringBuilder HeaderEditor(string title)
 		{
 			title = title.Trim();
-			title = title.ToLower();
-			string[] arrayTitle = title.Split(' ');
-			StringBuilder titleAdjustment = new StringBuilder();
-			arrayTitle[0] = char.ToUpper(arrayTitle[0][0]).ToString() + arrayTitle[0].Remove(0, 1);
-			arrayTitle[arrayTitle.Length - 1] = char.ToUpper(arrayTitle[arrayTitle.Length - 1][0]).ToString() + arrayTitle[arrayTitle.Length - 1].Remove(0, 1);
-			string[] exceptions = { "a", "an", "and", "at", "but", "by", "for", "in", "nor", "of", "on", "or", "out", "so", "the", "to", "up", "yet" };
-			for (int i = 0; i < arrayTitle.Length; ++i)
+			StringBuilder newTitle = new StringBuilder();
+			if (title.Length != 0)
 			{
-				bool result = true;
-				if (arrayTitle[i] != "")
+				title = title.ToLower();
+				string[] arrayTitle = title.Split(' ');
+				StringBuilder titleAdjustment = new StringBuilder();
+				arrayTitle[0] = char.ToUpper(arrayTitle[0][0]).ToString() + arrayTitle[0].Remove(0, 1);
+				arrayTitle[arrayTitle.Length - 1] = char.ToUpper(arrayTitle[arrayTitle.Length - 1][0]).ToString() + arrayTitle[arrayTitle.Length - 1].Remove(0, 1);
+				string[] exceptions = { "a", "an", "and", "at", "but", "by", "for", "in", "nor", "of", "on", "or", "out", "so", "the", "to", "up", "yet" };
+				for (int i = 0; i < arrayTitle.Length; ++i)
 				{
-					for (int j = 0; j < exceptions.Length - 1; ++j)
+					bool result = true;
+					if (arrayTitle[i] != "")
 					{
-						if (arrayTitle[i] == exceptions[j])
+						for (int j = 0; j < exceptions.Length - 1; ++j)
 						{
-							result = false;
-							break;
+							if (arrayTitle[i] == exceptions[j])
+							{
+								result = false;
+								break;
+							}
+						}
+						if (result)
+						{
+							arrayTitle[i] = char.ToUpper(arrayTitle[i][0]).ToString() + arrayTitle[i].Remove(0, 1);
+						}
+						titleAdjustment.Append(arrayTitle[i]);
+						titleAdjustment.Append(' ');
+					}
+				}
+				char[] punctuationMarks = { ',', ':', ';', '?', '.', '!', '-' };
+				for (int i = 0; i < titleAdjustment.Length - 1; ++i)
+				{
+					bool addSpace = false;
+					for (int j = 0; j < punctuationMarks.Length; ++j)
+					{
+						if (titleAdjustment[i] == ' ' && titleAdjustment[i + 1] == punctuationMarks[j])
+						{
+							i += 1;
+						}
+						if (titleAdjustment[i] == punctuationMarks[j] && titleAdjustment[i + 1] != ' ')
+						{
+							addSpace = true;
 						}
 					}
-					if (result)
+					if (titleAdjustment[i] == '-')
 					{
-						arrayTitle[i] = char.ToUpper(arrayTitle[i][0]).ToString() + arrayTitle[i].Remove(0, 1);
+						newTitle.Append(' ');
 					}
-					titleAdjustment.Append(arrayTitle[i]);
-					titleAdjustment.Append(' ');
-				}
-			}
-			char[] punctuationMarks = { ',', ':', ';', '?', '.', '!', '-' };
-			StringBuilder newTitle = new StringBuilder();
-			for (int i = 0; i < titleAdjustment.Length - 1; ++i)
-			{
-				bool addSpace = false;
-				for (int j = 0; j < punctuationMarks.Length; ++j)
-				{
-					if (titleAdjustment[i] == ' ' && titleAdjustment[i + 1] == punctuationMarks[j])
+					newTitle.Append(titleAdjustment[i]);
+					if (addSpace)
 					{
+						newTitle.Append(' ');
+						newTitle.Append(titleAdjustment[i + 1].ToString().ToUpper());
 						i += 1;
 					}
-					if (titleAdjustment[i] == punctuationMarks[j] && titleAdjustment[i + 1] != ' ')
-					{
-						addSpace = true;
-					}
-				}
-				if (titleAdjustment[i] == '-')
-				{
-					newTitle.Append(' ');
-				}
-				newTitle.Append(titleAdjustment[i]);
-				if (addSpace)
-				{
-					newTitle.Append(' ');
-					newTitle.Append(titleAdjustment[i + 1].ToString().ToUpper());
-					i += 1;
 				}
 			}
 			return newTitle;
